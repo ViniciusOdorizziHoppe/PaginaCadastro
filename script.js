03320a3d6ef73a2f3fa422ea1639c10dbc011cb6
ViniciusOdorizziHoppe <viniciusodorizzi25@gmail.com>
2025-06-07 20:04:52 -0300

    pegando as informações com Js

---
+++ script.js
@@ -0,0 +1,9 @@
+var TagEmail = document.getElementById("email")
+var EmailCadastrado = String(TagEmail.value)
+var TagSenha = document.getElementById("senha")
+var SenhaCadastrada = String(TagSenha.value)
+
+function CadastroUsuario() {
+    console.log(EmailCadastrado)
+    console.log(SenhaCadastrada)
+}
