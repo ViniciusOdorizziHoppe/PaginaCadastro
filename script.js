133ce32085584d6d77faca38641a639878f6b2ab
ViniciusOdorizziHoppe <viniciusodorizzi25@gmail.com>
2025-06-08 21:04:35 -0300

    página de login e cadastro feitas

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var TagEmail = document.getElementById("email")
-var EmailCadastrado = String(TagEmail.value)
-var TagSenha = document.getElementById("senha")
-var SenhaCadastrada = String(TagSenha.value)
 
 function CadastroUsuario() {
+
+    var TagEmail = document.getElementById("email")
+    var EmailCadastrado = String(TagEmail.value)
+    var TagSenha = document.getElementById("senha")
+    var SenhaCadastrada = String(TagSenha.value)
+
+    if (EmailCadastrado.length == 0 || SenhaCadastrada.length == 0) {
+        alert(`[ERRO] Confira se Informou os Campos`)
+    }
     console.log(EmailCadastrado)
     console.log(SenhaCadastrada)
+
+    if (EmailCadastrado.length != 0 && SenhaCadastrada.length != 0) {
+        window.location.href = "Login.html";   
+    }
+    
+}
+// Caso o usúario já tenha conta
+function PáginaDeLogin() {
+window.location.href = "Login.html"
+}
+
+// Verificação de usúario
+function LoginUsuario() {
+    console.log("TUDO CERTO CAMPEÂO")
 }
