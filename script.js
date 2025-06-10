e875958c041bde273c376ba5981c4d9378d3c79d
dasdwe23 <leohenriqueandrade.2010@gmail.com>
2025-06-10 09:45:28 -0300

    Feito parte de lucro

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function CadastroUsuario() {
     if (EmailCadastrado.length == 0 || SenhaCadastrada.length == 0) {
         alert(`[ERRO] Confira se Informou os Campos`)
     }
-    console.log(EmailCadastrado)
-    console.log(SenhaCadastrada)
 
     if (EmailCadastrado.length != 0 && SenhaCadastrada.length != 0) {
         window.location.href = "Login.html";   
@@ -24,5 +22,26 @@ window.location.href = "Login.html"
 
 // Verificação de usúario
 function LoginUsuario() {
+    window.location.href = "Home.html"
     console.log("TUDO CERTO CAMPEÂO")
+    
+}
+
+function LucroUsuario() {
+    var QTV = document.getElementById("QuantidadeVendas")
+    var QuantidadeVenda = Number(QTV.value)
+    var VLV = document.getElementById("ValorVendas")
+    var ValorVenda = Number(VLV.value)  
+    var PTV = document.getElementById("PercentualVendas")
+    var PercentualVenda = Number(PTV.value)  
+
+    var CalcFaturado = QuantidadeVenda * ValorVenda
+    var CalcPercent = PercentualVenda/100
+    var CalcLucrado = CalcFaturado * CalcPercent
+
+    var Lucro = document.getElementById("lucro")
+    Lucro.innerHTML = `${CalcLucrado}`
+    var Faturamento = document.getElementById("faturamento")
+    Faturamento.innerHTML = `${CalcFaturado}`
+
 }
